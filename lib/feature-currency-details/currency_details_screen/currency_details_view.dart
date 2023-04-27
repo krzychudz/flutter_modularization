a20511f4bb33b34eaaf3c0a5164b0340ca370592
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2023-04-27 21:41:21 +0200

    Display markets

--- lib/feature-currency-details/currency_details_screen/currency_details_view.dart
+++ lib/feature-currency-details/currency_details_screen/currency_details_view.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:flutter_modularization/core-data/extension/string_number.dart";
+import "package:flutter_modularization/data/models/exchange_market/exchange_market.dart";
 import "package:flutter_modularization/feature-currency-details/currency_details_screen/bloc/currency_details_screen_cubit.dart";
 import "package:flutter_modularization/feature-currency-details/currency_details_screen/bloc/currency_details_screen_state.dart";
 
@@ -26,7 +27,84 @@ class CurrencyDetailsViewBody extends StatelessWidget {
         slivers: [
           SliverHeader(header: 'Details'),
           CurrencyDetailsSection(),
+          SliverHeader(header: 'Markets'),
+          CurrencyMarketsSection()
         ],
+      ),
+    );
+  }
+}
+
+class CurrencyMarketsSection extends StatelessWidget {
+  const CurrencyMarketsSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CurrencyDetailsScreenCubit, CurrencyDetailsScreenState>(
+        buildWhen: (previous, current) =>
+            previous.exchangeMarkets != current.exchangeMarkets,
+        builder: (context, state) {
+          return state.exchangeMarkets == null
+              ? const ProgressIndicator()
+              : SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    childCount: state.exchangeMarkets?.length,
+                    (context, index) => MarketItem(
+                      exchangeMarket: state.exchangeMarkets?[index],
+                    ),
+                  ),
+                );
+        });
+  }
+}
+
+class MarketItem extends StatelessWidget {
+  const MarketItem({
+    super.key,
+    this.exchangeMarket,
+  });
+
+  final ExchangeMarket? exchangeMarket;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              exchangeMarket?.exchangeId ?? '',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 8),
+            Text(exchangeMarket?.quoteSymbol ?? ''),
+            const SizedBox(height: 8),
+            Text('${exchangeMarket?.priceUsd.fixedNumber(2)} USD'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProgressIndicator extends StatelessWidget {
+  const ProgressIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverToBoxAdapter(
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircularProgressIndicator(),
+        ),
       ),
     );
   }
